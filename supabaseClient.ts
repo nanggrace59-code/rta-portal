48c5ce3382501d0f2c610e1a4edc2ab8c876492e
nanggrace59-code <nanggrace59@gmail.com>
2026-01-08 20:52:41 +0700

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 // These environment variables must be set in your .env file or deployment settings
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || '';
-
+const supabaseUrl = "https://ojduzzmiyihgeuzoiynm.supabase.co" 
+const supabaseAnonKey = "eyJhbgCiOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qZHV6em1peWloZ2V1em9peW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzNTQ4NjMsImV4cCI6MjA1MTkzMDg2M30.Knjc4NzI4NjAsImV4cCI6MjA1MTkzMDg2M30..."
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
